b1bee03177544f945d4024da9782f625ac4e7cd3
Muhammad Aleem Taufiq <maleemtaufiq@gmail.com>
2020-09-28 14:31:11 +0200

    change

--- services/api.php
+++ services/api.php
@@ -1,8 +1,8 @@
 <?php
 require "../config/configuration.php";
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
+header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding,X-Requested-With");
 header("Content-type:application/json");
 $response = array();
 
